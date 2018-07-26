0007c95023b07fbbc39db4e5bf04b93536f56513
Akiyo <flying.unicorn.sf@gmail.com>
2018-07-26 12:18:14 -0700

    react app

--- js/react.jsx
+++ js/react.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {render} from 'react-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MainList from './components/MainList/MainList';
+import Detail from './pages/Detail';
 
-class App extends React.Component {
+class App extends Component {
   render () {
-    return <p> Hello React!</p>;
+    return (
+    <Router>
+      <div className="App">
+      <p> Hello React!</p>
+        <Route exact path="/" component={MainList} />
+        <Route exact path="/users/:id" component={Detail} />
+      </div>
+    </Router> 
+    );
   }
 }
 
